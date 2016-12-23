97075ec29c0b9720af5cc9035f7b7f8e13ba3fa5
Scott Chamberlain <myrmecocystus@gmail.com>
2016-12-23 11:27:29 -0800

    fix #15 - not using urltools for setting up query params    though issue opened din urltools repo so may move back to it if fix on cran bumped devv version added tests for query params

--- R/zzz.R
+++ R/zzz.R
@@ -9,11 +9,29 @@ make_url <- function(url = NULL, handle = NULL, path, query) {
     urltools::path(url) <- path
   }
 
-  if (length(query)) {
-    for (i in seq_along(query)) {
-      url <- urltools::param_set(url, names(query)[i], query[[i]])
-    }
-  }
+  url <- add_query(query, url)
+  # if (length(query)) {
+    # for (i in seq_along(query)) {
+    #   url <- urltools::param_set(url, names(query)[i], query[[i]])
+    # }
+  # }
 
   return(list(url = url, handle = handle$handle))
+}
+
+# query <- list(a = 5, a = 6)
+# query <- list(a = 5)
+# query <- list()
+# add_query(query, "https://httpbin.org")
+add_query <- function(x, url) {
+  if (length(x)) {
+    quer <- list()
+    for (i in seq_along(x)) {
+      quer[[i]] <- paste(names(x)[i], x[[i]], sep = "=")
+    }
+    parms <- paste0(quer, collapse = "&")
+    paste0(url, "?", parms)
+  } else {
+    return(url)
+  }
 }
