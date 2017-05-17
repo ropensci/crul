37aaddb7a7dd94af8422ac532f9ebf64d3a70329
Scott Chamberlain <myrmecocystus@gmail.com>
2017-05-16 22:43:54 -0700

    added email

--- revdep/check.R
+++ revdep/check.R
@@ -3,3 +3,5 @@ library("devtools")
 res <- revdep_check(threads = 4, type = "mac.binary")
 revdep_check_save_summary()
 revdep_check_print_problems()
+
+revdep_email(date = "May 17", version = "0.3.6", only_problems = FALSE, draft = TRUE)
