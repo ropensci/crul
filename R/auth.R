267a831304bcd3612dbce239a9f7ce35e92b3a8d
Scott Chamberlain <myrmecocystus@gmail.com>
2018-01-19 10:03:52 -0800

    fix #43 more docs on auth()

--- R/auth.R
+++ R/auth.R
@@ -1,12 +1,20 @@
 #' Authentication
 #'
 #' @export
-#' @param user (character) username, optional
-#' @param pwd (character) password, optional
+#' @param user (character) username, required. see Details.
+#' @param pwd (character) password, required. see Details.
 #' @param auth (character) authentication type, one of basic (default),
-#' digest, digest_ie, gssnegotiate, ntlm, or any. optional
+#' digest, digest_ie, gssnegotiate, ntlm, or any. required
+#' 
 #' @details
-#' Only supporting simple auth for now, OAuth later.
+#' Only supporting simple auth for now, OAuth later maybe.
+#' 
+#' For `user` and `pwd` you are required to pass in some value. 
+#' The value can be `NULL` to - which is equivalent to passing in an 
+#' empty string like `""` in `httr::authenticate`. You may want to pass
+#' in `NULL` for both `user` and `pwd` for example if you are using 
+#' `gssnegotiate` auth type. See example below.
+#' 
 #' @examples
 #' auth(user = "foo", pwd = "bar", auth = "basic")
 #' auth(user = "foo", pwd = "bar", auth = "digest")
@@ -28,6 +36,9 @@
 #'   auth = auth(user = "user", pwd = "passwd")
 #' ))
 #' res$auth
+#' 
+#' # gssnegotiate auth
+#' auth(NULL, NULL, "gssnegotiate")
 auth <- function(user, pwd, auth = "basic") {
   structure(ccp(list(
     userpwd = make_up(user, pwd),
