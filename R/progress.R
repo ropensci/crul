11084e9a8468ab9004ec2555a4bb112bbd8e0f7a
Scott Chamberlain <myrmecocystus@gmail.com>
2018-07-04 16:53:03 -0700

    man file progress, add egs to readme, #20

--- R/progress.R
+++ R/progress.R
@@ -1,34 +1,11 @@
-#' progress bar helper
+#' progress bars
 #'
 #' @name progress
-#' @details uses `httr::progress()` and pulls out that info 
-#' to pass down to \pkg{curl}
+#' @details pass `httr::progress()` to `progress` param in [HttpClient], 
+#' which pulls out relevant info to pass down to \pkg{curl}
 #' 
 #' if file sizes known you get progress bar; if file sizes not 
 #' known you get bytes downloaded
-#' @examples
-#' (x <- HttpClient$new(
-#'   url = "https://httpbin.org/bytes/102400", 
-#'   progress = httr::progress()
-#' ))
-#' z <- x$get()
-#' w <- x$post()
 #' 
-#' # with Paginator - Crossref API
-#' (cli <- HttpClient$new(url = "https://api.crossref.org", 
-#'   progress = httr::progress()))
-#' cc <- Paginator$new(client = cli, limit_param = "rows",
-#'    offset_param = "offset", limit = 50, limit_chunk = 10)
-#' cc
-#' cc$get('works')
-#' cc$responses()
-#' 
-#' # with Paginator - GBIF API
-#' (cli <- HttpClient$new(url = "https://api.gbif.org", 
-#'   progress = httr::progress()))
-#' cc <- Paginator$new(client = cli, limit_param = "limit",
-#'    offset_param = "offset", limit = 150, limit_chunk = 30)
-#' cc
-#' cc$get('v1/occurrence/search')
-#' cc$responses()
+#' See the README for examples
 NULL
