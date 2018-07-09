09ff3e4e3cda0324c42e06b65fdae16764f93647
Scott Chamberlain <myrmecocystus@gmail.com>
2018-07-09 14:19:34 -0700

    add missing param defs to args.R template file

--- man-roxygen/args.R
+++ man-roxygen/args.R
@@ -3,5 +3,10 @@
 #' \code{\link[curl]{curl_options}} for available curl options
 #' @param proxies an object of class \code{proxy}, as returned from the
 #' \code{\link{proxy}} function. Supports one proxy for now
+#' @param auth result of a call to the \code{\link{auth}} function, 
+#' e.g. \code{auth(user = "foo", pwd = "bar")}
 #' @param headers (list) a named list of headers
 #' @param handle A handle, see \code{\link{handle}}
+#' @param progress a function with logic for printing a progress
+#' bar for an HTTP request, ultimiately passed down to \pkg{curl}.
+#' only supports httr::progress() for now
