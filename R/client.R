2b86c65550f1dc260fd6ccd66904e33ddc831752
Scott Chamberlain <myrmecocystus@gmail.com>
2019-08-02 11:46:46 -0700

    remove environment for testing previous changes

--- R/client.R
+++ R/client.R
@@ -1,5 +1,3 @@
-stuff_env <- new.env()
-
 #' HTTP client
 #'
 #' @export
@@ -526,7 +524,6 @@ HttpClient <- R6::R6Class(
         }
       }
       # build response
-      stuff_env$z <- list(opts = opts, resp = resp, headers = headers)
       HttpResponse$new(
         method = opts$method,
         url = resp$url,
