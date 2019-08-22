ea5b42713eddd0d97ef04fb5e3ef08e374508266
Scott Chamberlain <myrmecocystus@gmail.com>
2019-08-21 19:39:33 -0700

    #119 content type methods for HttpResponse class added added tests for these methods added a separate manual file for content types

---
+++ R/content-types.R
@@ -0,0 +1,60 @@
+#' Working with content types
+#'
+#' The [HttpResponse] class holds all the responses elements for an HTTP
+#' request. This document details how to work specifically with the 
+#' content-type of the response headers
+#'
+#' @name content-types
+#' @section Content types:
+#' The "Content-Type" header in HTTP responses gives the media type of the
+#' response. The media type is both the data format and how the data is
+#' intended to be processed by a recipient. (modified from rfc7231)
+#'
+#' @section Behavior of the parameters HttpResponse raise_for_ct* methods:
+#'
+#' - type: (only applicable for the `raise_for_ct()` method): instead of
+#' using one of the three other content type methods for html, json, or xml,
+#' you can specify a mime type to check, any of those in [mime::mimemap]
+#' - charset: if you don't give a value to this parameter, we only
+#' check that the content type is what you expect; that is, the charset,
+#' if given, is ignored.
+#' - behavior: by default when you call this method, and the content type
+#' does not match what the method expects, then we run `stop()` with a
+#' message. Instead of stopping, you can choose `behavior="warning"`
+#' and we'll throw a warning instead, allowing any downstream processing
+#' to proceed.
+#'
+#' @references
+#' spec for content types:
+#' <https://tools.ietf.org/html/rfc7231#section-3.1.1.5>
+#'
+#' spec for media types:
+#' <https://tools.ietf.org/html/rfc7231#section-3.1.1.1>
+#'
+#' @seealso [HttpResponse]
+#' @examples \dontrun{
+#' (x <- HttpClient$new(url = "https://httpbin.org"))
+#' (res <- x$get())
+#'
+#' ## get the content type
+#' res$response_headers$`content-type`
+#'
+#' ## check that the content type is text/html
+#' res$raise_for_ct_html()
+#'
+#' ## it's def. not json
+#' # res$raise_for_ct_json()
+#'
+#' ## give custom content type
+#' res$raise_for_ct("text/html")
+#' # res$raise_for_ct("application/json")
+#' # res$raise_for_ct("foo/bar")
+#'
+#' ## check charset in addition to the media type
+#' res$raise_for_ct_html(charset = "utf-8")
+#' # res$raise_for_ct_html(charset = "utf-16")
+#'
+#' # warn instead of stop
+#' res$raise_for_ct_json(behavior = "warning")
+#' }
+NULL
---
+++ tests/testthat/test-content-type.R
@@ -0,0 +1,99 @@
+skip_on_cran()
+
+res_html <- HttpClient$new(url = "https://httpbin.org")$get()
+res_json <- HttpClient$new(url = "https://httpbin.org/json")$get()
+res_xml <- HttpClient$new(url = "https://httpbin.org/xml")$get()
+
+test_that("html", {
+  ## get the content type
+  expect_match(res_html$response_headers$`content-type`,
+    "text/html; charset=utf-8")
+
+  ## check that the content type is text/html
+  expect_null(res_html$raise_for_ct_html())
+
+  ## it's def. not json or xml
+  expect_error(res_html$raise_for_ct_json(), "did not match")
+  expect_error(res_html$raise_for_ct_xml(), "did not match")
+  ### behavior: warning
+  expect_warning(res_html$raise_for_ct_json(behavior = "warning"),
+    "did not match")
+
+  ## give custom content type
+  expect_null(res_html$raise_for_ct("text/html"))
+  expect_error(res_html$raise_for_ct("application/json"),
+    "did not match")
+  ### behavior: warning
+  expect_warning(res_html$raise_for_ct("application/json", behavior = "warning"),
+    "did not match")
+  expect_error(res_html$raise_for_ct("foo/bar"),
+    "type not in allowed set")
+})
+
+test_that("json", {
+  ## get the content type
+  expect_match(res_json$response_headers$`content-type`,
+    "application/json")
+
+  ## check that the content type is text/html
+  expect_null(res_json$raise_for_ct_json())
+
+  ## it's def. not xml
+  expect_error(res_json$raise_for_ct_xml(), "did not match")
+  ### behavior: warning
+  expect_warning(res_json$raise_for_ct_xml(behavior = "warning"),
+    "did not match")
+
+  ## give custom content type
+  expect_null(res_json$raise_for_ct("application/json"))
+  expect_error(res_json$raise_for_ct("application/xml"),
+    "did not match")
+  ### behavior: warning
+  expect_warning(res_json$raise_for_ct("application/xml", behavior = "warning"),
+    "did not match")
+})
+
+test_that("xml", {
+  ## get the content type
+  expect_match(res_xml$response_headers$`content-type`, "application/xml")
+
+  ## check that the content type is text/html
+  expect_null(res_xml$raise_for_ct_xml())
+
+  ## it's def. not json
+  expect_error(res_xml$raise_for_ct_json(), "did not match")
+  ### behavior: warning
+  expect_warning(res_xml$raise_for_ct_json(behavior = "warning"),
+    "did not match")
+
+  ## give custom content type
+  expect_null(res_xml$raise_for_ct("application/xml"))
+  expect_error(res_xml$raise_for_ct("application/json"),
+    "did not match")
+  ### behavior: warning
+  expect_warning(res_xml$raise_for_ct("application/json", behavior = "warning"),
+    "did not match")
+  expect_error(res_xml$raise_for_ct("foo/bar"),
+    "type not in allowed set")
+})
+
+
+test_that("charset works", {
+  ## check charset in addition to the media type
+
+  ### warning thrown that no charset detected - don't want to fail if no charset given by server
+  expect_warning(res_json$raise_for_ct_json(charset = "utf-8"),
+    "no charset detected")
+
+  ### charset should be given with text/html response
+  #### nothing happens if a match
+  expect_null(res_html$raise_for_ct_html(charset = "utf-8"))
+  #### error if not matched
+  expect_error(res_html$raise_for_ct_html(charset = "utf-16"),
+    "did not match")
+
+  ### w/ custom type
+  expect_null(res_html$raise_for_ct("text/html", charset = "utf-8"))
+  expect_error(res_html$raise_for_ct("text/html", charset = "utf-16"),
+    "did not match")
+})
