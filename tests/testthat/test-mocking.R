6305fdfbea3fadd8f3ddb0bdec268bc44ff5604b
Scott Chamberlain <sckott@fastmail.com>
2024-07-18 15:09:34 -0700

    REVDEPCHECK add tests for Async and mocking

--- tests/testthat/test-mocking.R
+++ tests/testthat/test-mocking.R
@@ -41,14 +41,57 @@ test_that("mocking with HttpRequest", {
   bb <- make_req(url)
 
   # content and times differ btw the two
-  expect_is(aa, "HttpResponse")
-  expect_is(bb, "HttpResponse")
+  expect_s3_class(aa, "HttpResponse")
+  expect_s3_class(bb, "HttpResponse")
 
   expect_is(aa$content, "raw")
   expect_equal(length(bb$content), 0)
 
   expect_is(aa$times, "numeric")
   expect_null(bb$times)
+
+  # clean up
+  webmockr::stub_registry_clear()
+})
+
+context("mocking: HttpRequest with Async")
+test_that("mocking with HttpRequest", {
+  skip_if_not_installed("webmockr")
+
+  loadNamespace("webmockr")
+  url <- hb()
+  urls <- c(
+    file.path(url, "get"),
+    file.path(url, "anything"),
+    file.path(url, "encoding/utf8")
+  )
+  for (u in urls) webmockr::stub_request("get", u)
+  # webmockr::stub_registry()
+
+  make_req <- function(urls) {
+    cc <- Async$new(urls = urls)
+    cc$get()
+  }
+
+  mock(FALSE)
+  # webmockr IS NOT enabled
+  not_mocked <- make_req(urls)
+
+  # webmockr IS enabled
+  mock()
+  mocked <- make_req(urls)
+
+  expect_s3_class(not_mocked[[1]], "HttpResponse")
+  expect_s3_class(mocked[[1]], "HttpResponse")
+
+  expect_s3_class(not_mocked[[1]]$status_http(), "http_code")
+  expect_s3_class(mocked[[1]]$status_http(), "http_code")
+
+  expect_is(not_mocked[[1]]$content, "raw")
+  expect_equal(length(mocked[[1]]$content), 0)
+
+  expect_is(not_mocked[[1]]$times, "numeric")
+  expect_null(mocked[[1]]$times)
 
   # clean up
   webmockr::stub_registry_clear()
